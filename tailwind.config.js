e391833125d0600125a99d0aa6900d63802c58f9
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-06-05 19:56:38 +0500

    initial canges maked and logos + images copyed from figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors:{
+      ...colors,
+      'primary': '#20CE94',
+      'primary-dark': '#2BBE8D',
+      'primary-light': 'rgba(43, 190, 141, 0.15)',
+      'secondary': '#323130',
+      'active-color': '#8577E6',
+      'border-color': '#F5F5F5',
+      'bg_gray': '#DCDEE3'
+    }
   },
   plugins: [],
-}
-
+};
