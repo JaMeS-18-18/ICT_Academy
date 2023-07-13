cef7a0b2abf6815433ee4f580d319c1cff68ca87
JaloliddinRuzikulov <ruzikulov@tuit.uz>
2023-07-13 14:43:09 +0000

    Update some

--- src/components/Info_section/index.jsx
+++ src/components/Info_section/index.jsx
@@ -25,7 +25,7 @@ return (
      <m.div  className='flex col-span-12 md:col-span-6 lg:col-span-12 items-center my-4 md:my-6'>
         <GraduetesIcon />
         <div style={{width: "100%"}} className="ms-5 ps-5 text-start ">
-          <m.h4 initial={{y: "200%"}} animate={{y:0}} transition={{duration: 0.75}}  className='text-xl font-semibold'>{About?.graduates} muvaffaqiyatli bitiruvchi</m.h4>
+          <m.h4 initial={{y: "200%"}} animate={{y:0}} transition={{duration: 0.75}}  className='text-xl font-semibold'>{About?.graduates}+ muvaffaqiyatli bitiruvchi</m.h4>
           <m.p initial={{y: "200%"}} animate={{y:0}} transition={{duration: 0.75}} className='text-l text-secondary'>IT sohasida o’z o’rniga ega</m.p>
         </div>
       </m.div>
@@ -59,11 +59,11 @@ return (
       <m.img initial={{x: "100%"}} animate={{x:0}} transition={{duration: 0.75}} src={SectionImage} alt="" />
       <div className='statistik bg-primary '>
         <div className="card-1 md:border-e-2 w-32">
-          <h2 className='text-2xl text-center md:text-4xl font-semibold'>{About?.courses}</h2>
+          <h2 className='text-2xl text-center md:text-4xl font-semibold'>{About?.course_directions}</h2>
           <p className='text-center'>Kurslar</p>
         </div>
         <div className="card-2 md:border-e-2 w-32 text-start">
-          <h2 className='text-2xl text-center md:text-start md:text-4xl font-semibold'>{About?.graduates}</h2>
+          <h2 className='text-2xl text-center md:text-start md:text-4xl font-semibold'>{About?.graduates}+</h2>
           <p className='text-center md:text-start'>Bitiruvchilar</p>
         </div>
         <div className="card-3 w-32 text-start">
